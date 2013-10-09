c57131bbfcf96f09eed5ac9993a271e25077166f
merrick1988 <merrick198@gmail.com>
2013-10-09 13:54:57 +0300

    add Create Contact Form

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,9 @@
 	angular.module('mainModule', [])
 		.controller("mainController", ['$scope', 'contactService', function($scope, contactService){
 			$scope.contacts = [];
+			$scope.newContact = null;
 
-			var loadContacts, init;
+			var loadContacts, init, onAddContactSuccess;
 
 			init = function(){
 				loadContacts();
@@ -13,6 +14,19 @@
 			loadContacts = function(){
 				$scope.contacts = contactService.loadContacts();
 			};
+			onAddContactSuccess = function(){
+				$scope.newContact = null;
+				alertify.success("Contact was added successfully");
+			}
+
+			$scope.showAddContact = function(){
+				$scope.newContact = {};
+			};
+
+			$scope.addContact = function(){
+				if(!$scope.newContact) return;
+				contactService.addContact($scope.newContact, onAddContactSuccess);
+			}
 
 			init();
 		}])
--- scripts/services/contactService.js
+++ scripts/services/contactService.js
@@ -4,28 +4,34 @@
 	angular.module("mainModule").
 		service("contactService",[ function(){
 			var self = this;
+			    self.contacts;
 
 			self.loadContacts =  function(){
-				var contacts = {
-					0: {
+				self.contacts = [
+					{
 						name: "First Contact",
 						phone: "111-111-11"
 					},
-					1: {
+					{
 						name: "Second Contact",
 						phone: "222-222-22"
 					},
-					2: {
+					{
 						name: "Third Contact",
 						phone: "333-333-33"
 					},
-					3: {
+					{
 						name: "Fourth Contact",
 						phone: "444-444-44"
 					}
-				}
+				]
 
-				return contacts;
+				return self.contacts;
 			}
+			self.addContact =  function(contact, success){
+				self.contacts.push(contact);
+				success && success();
+			};
+
 		}])
 })();

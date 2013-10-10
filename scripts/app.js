f00e75ee06c343487085c8dca1d404f20123aafd
merrick1988 <merrick198@gmail.com>
2013-10-10 18:10:49 +0300

    add edit and delete contacts

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,7 @@
 (function(){
 	'use strict';
 	angular.module('mainModule', [])
-		.controller("mainController", ['$scope', 'contactService', function($scope, contactService){
-			$scope.contacts = [];
-			$scope.newContact = null;
-
-			var loadContacts, init, onAddContactSuccess;
-
-			init = function(){
-				loadContacts();
-			};
-
-			loadContacts = function(){
-				$scope.contacts = contactService.loadContacts();
-			};
-			onAddContactSuccess = function(){
-				$scope.newContact = null;
-				alertify.success("Contact was added successfully");
-			}
-
-			$scope.showAddContact = function(){
-				$scope.newContact = {};
-			};
-
-			$scope.addContact = function(){
-				if(!$scope.newContact) return;
-				contactService.addContact($scope.newContact, onAddContactSuccess);
-			}
-
-			init();
+		.controller("mainController", ['$scope', function($scope){
 		}])
 })();
 
--- scripts/services/contactService.js
+++ scripts/services/contactService.js
@@ -2,25 +2,29 @@
 	'use strict';
 
 	angular.module("mainModule").
-		service("contactService",[ function(){
+		service("contactService", ['$timeout', function($timeout){
 			var self = this;
 			    self.contacts;
 
 			self.loadContacts =  function(){
 				self.contacts = [
 					{
+						id: "1",
 						name: "First Contact",
 						phone: "111-111-11"
 					},
 					{
+						id: "2",
 						name: "Second Contact",
 						phone: "222-222-22"
 					},
 					{
-						name: "Third Contact",
+						id: "3",
+						name: "Third contact",
 						phone: "333-333-33"
 					},
 					{
+						id: "4",
 						name: "Fourth Contact",
 						phone: "444-444-44"
 					}
@@ -31,6 +35,30 @@
 			self.addContact =  function(contact, success){
 				self.contacts.push(contact);
 				success && success();
+			};
+
+			self.saveContact =  function(contact, success){
+				var index = '';
+				_.each(self.contacts,function(item, i){
+					if(item.id == contact.id){
+						index = i;
+					}
+				});
+				self.contacts[index]  = angular.copy(contact);
+				success && success();
+
+			};
+
+			self.deleteContact =  function(contact, success){
+				var index = '';
+				_.each(self.contacts,function(item, i){
+					if(item.id == contact.id){
+						index = i;
+					}
+				});
+				self.contacts.splice(index, 1);
+				success && success();
+
 			};
 
 		}])
